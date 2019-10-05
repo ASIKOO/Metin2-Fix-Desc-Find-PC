77521581323e64db05ed0749ad79f35f6c66df95
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2019-10-05 18:06:18 +0300

    Add files via upload

---
+++ game/src/desc_manager.cpp
@@ -0,0 +1,16 @@
+///Add
+#include <boost/algorithm/string.hpp>
+
+//Find
+	if (d->GetCharacter() && !strcmp(d->GetCharacter()->GetName(), m_name))
+			return true;
+		return false;
+		
+///Change
+#if defined(FIXDESCFINDPC)
+		return d->GetCharacter() && boost::algorithm::to_lower_copy(std::string(d->GetCharacter()->GetName())) == boost::algorithm::to_lower_copy(std::string(m_name));
+#else
+		if (d->GetCharacter() && !strcmp(d->GetCharacter()->GetName(), m_name))
+			return true;
+		return false;
+#endif
